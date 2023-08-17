c88c6cf4e897dbc182e413b0a3e42b5d7fd91ea4
Karr4 but from another universe <XGon20987@gmail.com>
2023-08-17 10:09:22 +0300

    fixing issues

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,11 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [
-  { id: 'serega', name: 'Serega Bublik', number: '228-69-47' },
-  { id: 'id-2', name: 'Harry Squater', number: '239-93-21' },
-  { id: 'id-3', name: 'Captain Jacked Sparrow', number: '432-34-56' },
-  { id: 'id-4', name: 'Harry Shprotter', number: '432-98-09' },
-];
+const initialState = [];
 
 const contactsSlice = createSlice({
   name: 'contacts',
